c0efa2aa8c5c0e9d885926bbe2056efae8f72115
hugoriv11 <37352235+hugoriv11@users.noreply.github.com>
2018-03-14 12:32:26 -0600

    Create Programa1.java

---
+++ Programa1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programa1;
+
+public class Programa1 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("++++++++ Mi info  ++++++++");
+        System.out.println("Nombre: HUGO ROLANDO RIVERA MENJIVAR");
+        System.out.println("Edad: 19");
+        System.out.println("Fecha de Nac: 15/11/98");
+        System.out.println("Deporte fav: Fut");
+        System.out.println("Musica: De todo menos rancheras o banda");
+        System.out.println(".:~~~~~~~~~~~~~ Hasta aqui quedo ~~~~~~~~~~~~:.");
+    }
+    
+}
